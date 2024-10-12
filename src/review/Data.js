4f2a912f8bf091fde927dead5a735427ad21cb5d
Gourav Singh Bais <gouravbais08@Gouravs-MacBook-Air.local>
2024-10-13 01:53:26 +0530

    add new articles

--- src/review/Data.js
+++ src/review/Data.js
@@ -4,21 +4,21 @@ const reviews = [
       name: "Lokesh Pawar",
       job: "Software Engineer",
       image: "https://picsum.photos/seed/picsum/200/300",
-      text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+      text: "I have had the pleasure of working with Gourav on several projects, and I am consistently impressed with his technical expertise and ability to deliver quality data science and machine learning solutions on time and within budget. He is a true partner and an asset to any project.",
     },
     {
       id: 2,
       name: "Vivek Jhooriya",
       job: "Graphic Designer",
       image: "https://picsum.photos/seed/picsum/200/300",
-      text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+      text: "I have been working with Gourav for several years now, and I have always been impressed with his creativity and attention to details. He is true professional who takes pride in his work and always goes above and beyond to deliver exceptional results.",
     },
     {
       id: 3,
-      name: "Arjit Singh",
-      job: "Marketing Manager",
+      name: "Aadesh Gautam",
+      job: "Senior Data Scientist",
       image: "https://picsum.photos/seed/picsum/200/300",
-      text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+      text: "I am thrilled with the results of our recent machine learning project, and it wouldn't have been possible without the hard work and dedication of Gourav. Thank you for your exceptional work!",
     },
   ];
   
