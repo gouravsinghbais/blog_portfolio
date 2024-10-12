a27cfabf74f52246c5cbb513a4f3859111604760
Gourav Singh Bais <gouravbais08@Gouravs-MacBook-Air.local>
2024-10-12 19:10:53 +0530

    update account

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import BlogPost from './components/blog/BlogPost';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const App = () => {
-  const TRACKING_ID ="G-51X21L9TRF";
+  const TRACKING_ID = "G-51X21L9TRF";
 
-  useEffect(()=> {
-    ReactGA.initialize(TRACKING_ID);
+  useEffect(() => {
+    ReactGA.initialize({
+      trackingId: "G-51X21L9TRF"
+    });
+
+    ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
+
+    // Send a custom event
+    ReactGA.event({
+      category: "your category",
+      action: "your action",
+      label: "your label", // optional
+      value: 99, // optional, must be a number
+      nonInteraction: true, // optional, true/false
+      transport: "xhr", // optional, beacon/xhr/image
+    });
 
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, [])
